b863f210745c9a8ce09d166dca832db87905da1a
園田 亮平 <ryoheisonoda@outlook.com>
2023-10-16 02:01:41 +0900

    編集距離のアルゴリズム修正

--- packages/lib/array-util.test.ts
+++ packages/lib/array-util.test.ts
@@ -1,6 +1,43 @@
-import { shuffle, sortInner, splitChunks } from "./array-util";
+import {
+    sum,
+    shuffle,
+    sortInner,
+    splitChunks,
+    sortBySumOrFirst,
+    sortMatrix,
+    calculateEditDistance,
+} from "./array-util";
 
 describe("array-util", () => {
+    test("chunk", () => {
+        const array = [...Array(9).keys()].map((_, i) => i + 1);
+        const chunks = splitChunks(array, 4);
+        expect(chunks).toEqual([[1, 2, 3, 4], [5, 6, 7, 8], [9]]);
+    });
+
+    test("shuffle", () => {
+        const array = [...Array(9).keys()].map((_, i) => i + 1);
+        const shuffled = shuffle(array);
+        expect(shuffled).not.toEqual(array);
+    });
+
+    test("sum", () => {
+        expect(sum([1, 2, 3])).toEqual(6);
+    });
+
+    test("sortBySumOrFirst", () => {
+        const matrix = shuffle([
+            [5, 6, 7, 8],
+            [1, 2, 3, 9],
+            [3, 3, 3, 6],
+        ]);
+        expect(sortBySumOrFirst(matrix)).toEqual([
+            [1, 2, 3, 9],
+            [3, 3, 3, 6],
+            [5, 6, 7, 8],
+        ]);
+    });
+
     test("sortInner", () => {
         const array = [
             [5, 4, 3, 2],
@@ -12,15 +49,31 @@ describe("array-util", () => {
         ]);
     });
 
-    test("chunk", () => {
-        const array = [...Array(9).keys()].map((_, i) => i + 1);
-        const chunks = splitChunks(array, 4);
-        expect(chunks).toEqual([[1, 2, 3, 4], [5, 6, 7, 8], [9]]);
+    test("sortMatrix", () => {
+        const matrix = shuffle([
+            [8, 5, 6, 7],
+            [3, 9, 1, 2],
+            [3, 6, 3, 3],
+        ]);
+        expect(sortMatrix(matrix)).toEqual([
+            [1, 2, 3, 9],
+            [3, 3, 3, 6],
+            [5, 6, 7, 8],
+        ]);
     });
 
-    test("shuffle", () => {
-        const array = [...Array(9).keys()].map((_, i) => i + 1);
-        const shuffled = shuffle(array);
-        expect(shuffled).not.toEqual(array);
+    test("calculateEditDistance", () => {
+        expect(
+            calculateEditDistance(
+                [
+                    [1, 2, 3, 4],
+                    [5, 6, 7, 8],
+                ],
+                [
+                    [9, 10, 11, 12],
+                    [13, 14, 15, 16],
+                ],
+            ),
+        ).toEqual(8);
     });
 });
--- packages/lib/array-util.ts
+++ packages/lib/array-util.ts
@@ -1,4 +1,4 @@
-export function shuffle(array: number[]): number[] {
+export function shuffle<T>(array: T[]): T[] {
     const result = [...array];
     for (let i = result.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -7,8 +7,8 @@ export function shuffle(array: number[]): number[] {
     return result;
 }
 
-export function splitChunks(array: number[], chunkSize: number): number[][] {
-    const result: number[][] = [];
+export function splitChunks<T>(array: T[], chunkSize: number): T[][] {
+    const result: T[][] = [];
     for (let i = 0; i < array.length; i += chunkSize) {
         result.push(array.slice(i, i + chunkSize));
     }
@@ -23,31 +23,56 @@ export function pickMaxValueIndex(array: number[]): number {
     return biggestIndex;
 }
 
-export function sortInner(array: number[][]): number[][] {
+export function sortInner<T>(array: T[][]): T[][] {
     return array.map((items) => [...items].sort());
 }
 
+export function sum(array: number[]): number {
+    return array.reduce((sum, current) => sum + current, 0);
+}
+
+export function sortBySumOrFirst(array: number[][]): number[][] {
+    return array.sort((a, b) => {
+        const sumA = sum(a);
+        const sumB = sum(b);
+        if (sumA === sumB) {
+            return a[0] - b[0];
+        }
+        return sumA - sumB;
+    });
+}
+
+export function sortMatrix(matrix: number[][]): number[][] {
+    return sortBySumOrFirst(sortInner(matrix));
+}
+
 export function calculateEditDistance(arr1: number[][], arr2: number[][]): number {
-    return _calculateEditDistance(arr1.flat().sort(), arr2.flat().sort());
+    return _calculateEditDistance(sortMatrix(arr1).flat(), sortMatrix(arr2).flat());
 }
 
 function _calculateEditDistance(arr1: number[], arr2: number[]): number {
     const m = arr1.length;
     const n = arr2.length;
     const dp: number[][] = [];
+
     for (let i = 0; i <= m; i++) {
         dp[i] = [];
-        for (let j = 0; j <= n; j++) {
-            if (i === 0) {
-                dp[i][j] = j;
-            } else if (j === 0) {
-                dp[i][j] = i;
-            } else if (arr1[i - 1] === arr2[j - 1]) {
+        dp[i][0] = i;
+    }
+
+    for (let j = 0; j <= n; j++) {
+        dp[0][j] = j;
+    }
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (arr1[i - 1] === arr2[j - 1]) {
                 dp[i][j] = dp[i - 1][j - 1];
             } else {
-                dp[i][j] = 1 + Math.min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]);
+                dp[i][j] = Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + 1);
             }
         }
     }
+
     return dp[m][n];
 }
