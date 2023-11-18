cb3d1d06ec7cd5e166c1a4970c2a588fbe56b870
園田 亮平 <ryoheisonoda@outlook.com>
2023-11-19 00:20:34 +0900

    メッセージ微修正

--- packages/web/src/components/setting/RandomModeInput.tsx
+++ packages/web/src/components/setting/RandomModeInput.tsx
@@ -13,9 +13,9 @@ type Props = { value: RandomMode; onChange: (mode: RandomMode) => void };
 
 const descriptions: Record<RandomMode, string> = {
   DISCRETENESS:
-    "なるべく似通った面子にならないようにメンバーを選出します。連続での休憩が発生しやすいですが、何度も繰り返しているうちに平準化されていきます。",
+    "なるべく似通った面子にならないようにメンバーを選出します。連続での休憩が発生しますが、何度も繰り返しているうちに平準化されていきます。",
   EVENNESS:
-    "なるべく均等な回数でコートに入れるようにメンバーを選出します。連続で休憩が発生しづらくなりますが、似通った面子になる傾向が強くなります。",
+    "なるべく均等な回数でコートに入れるようにメンバーを選出します。連続での休憩が発生しづらくなりますが、似通った面子になる傾向が強くなります。",
 };
 
 export default function RandomModeInput({ value, onChange }: Props) {
