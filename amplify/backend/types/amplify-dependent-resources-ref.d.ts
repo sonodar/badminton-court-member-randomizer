dd1094a0c5ac12e174fa39125ed37c00e3503f87
園田 亮平 <ryoheisonoda@outlook.com>
2023-11-20 23:25:37 +0900

    amplify pull

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,18 +1,18 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "DoublesMemberGenerator": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
+    "api": {
+        "DoublesMemberGenerator": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "function": {
+        "DoublesMemberGeneratorEventCleaner": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string",
+            "LambdaExecutionRoleArn": "string"
+        }
     }
-  },
-  "function": {
-    "DoublesMemberGeneratorEventCleaner": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  }
 }
