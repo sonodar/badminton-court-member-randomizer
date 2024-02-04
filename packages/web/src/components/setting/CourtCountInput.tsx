1c38132d72c4213cdcc1e83a8cf3c5be112b8d76
園田 亮平 <ryohei-sonoda@m3.com>
2024-02-05 01:23:24 +0900

    コート数の初期値が反映されないバグを修正

--- packages/web/src/components/setting/CourtCountInput.tsx
+++ packages/web/src/components/setting/CourtCountInput.tsx
@@ -1,71 +1,59 @@
-import { Box, HStack, Stack, useRadio, useRadioGroup } from "@chakra-ui/react";
-import { COURT_COUNT_LIMIT } from "@doubles-member-generator/manager";
+import { Box, HStack, useRadio, useRadioGroup } from "@chakra-ui/react";
+import { array, COURT_COUNT_LIMIT } from "@doubles-member-generator/manager";
 import React from "react";
-
-function CourtCountButton(
-  props: Parameters<typeof useRadio>[0] & { children: React.ReactNode },
-) {
-  const { getInputProps, getRadioProps } = useRadio(props);
-
-  const input = getInputProps();
-  const checkbox = getRadioProps();
-
-  return (
-    <Box as="label">
-      <input {...input} />
-      <Box
-        {...checkbox}
-        cursor="pointer"
-        borderWidth="1px"
-        borderRadius="sm"
-        boxShadow="md"
-        sx={{ "--chakra-shadows-outline": "0" }}
-        _checked={{
-          bg: "brand.500",
-          color: "white",
-          borderColor: "brand.500",
-        }}
-        _focus={{
-          boxShadow: "outline",
-        }}
-        px={4}
-        py={2}
-      >
-        {props.children}
-      </Box>
-    </Box>
-  );
-}
+import type { UseRadioProps } from "@chakra-ui/radio";
 
 type Props = {
   value: number;
   onChange: (i: number) => void;
 };
 
+const COURT_IDS = array.generate(COURT_COUNT_LIMIT).map(String);
+
 export function CourtCountInput({ value, onChange }: Props) {
-  const numbers = Array.from(
-    { length: COURT_COUNT_LIMIT },
-    (_, i) => `${i + 1}`,
-  );
+  function CourtCountButton(props: UseRadioProps) {
+    const { getInputProps, getRadioProps } = useRadio(props);
+
+    return (
+      <Box as="label" cursor="pointer">
+        <input {...getInputProps()} hidden />
+        <Box
+          {...getRadioProps()}
+          borderWidth="1px"
+          borderRadius="sm"
+          boxShadow="md"
+          sx={{ "--chakra-shadows-outline": "0" }}
+          _checked={{
+            bg: "brand.500",
+            color: "white",
+            borderColor: "brand.500",
+          }}
+          _focus={{
+            boxShadow: "outline",
+          }}
+          px={4}
+          py={2}
+        >
+          {props.value}
+        </Box>
+      </Box>
+    );
+  }
 
   const { getRootProps, getRadioProps } = useRadioGroup({
     name: "courtCount",
-    defaultValue: value.toString(),
+    value: value.toString(),
     onChange: (e) => onChange(parseInt(e, 10)),
   });
 
   return (
-    <Stack>
-      <HStack {...getRootProps()}>
-        {numbers.map((value) => {
-          const radio = getRadioProps({ value });
-          return (
-            <CourtCountButton key={value} {...radio}>
-              {value}
-            </CourtCountButton>
-          );
-        })}
-      </HStack>
-    </Stack>
+    <HStack {...getRootProps()}>
+      {COURT_IDS.map((courtId) => (
+        <CourtCountButton
+          key={courtId}
+          {...getRadioProps({ value: courtId })}
+        />
+      ))}
+    </HStack>
   );
 }
