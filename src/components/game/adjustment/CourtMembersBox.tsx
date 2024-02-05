6aae4bf4a7afef7460c29b6963ea29e0c9d7f167
園田 亮平 <ryohei-sonoda@m3.com>
2024-02-06 01:48:44 +0900

    続リファクタリング

---
+++ src/components/game/adjustment/CourtMembersBox.tsx
@@ -0,0 +1,40 @@
+import { Box, SimpleGrid } from "@chakra-ui/react";
+import React from "react";
+import { MemberDroppable } from "./MemberDroppable.tsx";
+import { MemberBox } from "./MemberBox.tsx";
+import type { CourtMembers } from "@logic";
+
+type Props = {
+  courtId: number;
+  courtMembers: CourtMembers;
+};
+
+export function CourtMembersBox({ courtId, courtMembers }: Props) {
+  return (
+    <Box
+      borderStyle={"solid"}
+      borderWidth={1}
+      borderRadius={"md"}
+      py={1}
+      pl={1}
+    >
+      <SimpleGrid columns={4} spacing={0}>
+        {courtMembers.map((memberId) => (
+          <MemberDroppable
+            key={memberId}
+            type={"courtMember"}
+            courtId={courtId}
+            memberId={memberId}
+          >
+            <MemberBox
+              type={"courtMember"}
+              color={"brand.300"}
+              courtId={courtId}
+              memberId={memberId}
+            />
+          </MemberDroppable>
+        ))}
+      </SimpleGrid>
+    </Box>
+  );
+}
