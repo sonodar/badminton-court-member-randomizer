6b3ba8e4d55f54d0b970a6249907e698b4512bb3
園田 亮平 <ryohei-sonoda@m3.com>
2024-02-06 14:10:44 +0900

    wip: 軽量化

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,28 @@ export default defineConfig({
   adapter: node({ mode: "standalone" }),
   integrations: [react()],
   vite: {
+    ssr: {
+      noExternal: [
+        "@astrojs/node",
+        "@astrojs/react",
+        "@chakra-ui/icons",
+        "@chakra-ui/react",
+        "@dnd-kit/core",
+        "@emotion/react",
+        "@emotion/styled",
+        "astro",
+        "aws-amplify",
+        "framer-motion",
+        "jotai",
+        "ms",
+        "react",
+        "react-dom",
+        "react-icons",
+        "ts-pattern",
+        "typescript",
+        "zod",
+      ],
+    },
     define: {
       "window.global": {},
     },
--- build-for-amplify-hosting.sh
+++ build-for-amplify-hosting.sh
@@ -13,7 +13,10 @@ mv dist/server .amplify-hosting/compute/default
 cp deploy-manifest.json .amplify-hosting/deploy-manifest.json
 
 # node_modules を軽量化する（ただし、node_modules はキャッシュするのでそのままにしておく）
-yarn install --production --modules-folder node_modules_production
+rm -rf node_modules_production
+yarn install --production --ignore-optional --ignore-scripts --modules-folder node_modules_production
+# なぜか typescript や babel が含まれているので削除 (30Mもある)
+rm -rf node_modules_production/{typescript,@babel}
 
 rm -rf .amplify-hosting/compute/default/node_modules
 mv node_modules_production .amplify-hosting/compute/default/node_modules
